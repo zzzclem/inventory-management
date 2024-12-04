05cb7992293977c400edf83b9561a691de06cb49
zzzclem <143154954+zzzclem@users.noreply.github.com>
2024-12-04 13:58:39 +0800

    initial commit

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [
-        {
-            name: "inventory-management",
-            scripts: "npm",
-            args: "run dev",
-            env: {
-                NODE_ENV: "development",
-                ENV_VAR1: "environment-variable",
-            },
+      {
+        name: "inventory-management",
+        script: "npm",
+        args: "run dev",
+        env: {
+          NODE_ENV: "development",
+          ENV_VAR1: "environment-variable",
         },
+      },
     ],
-};
+  };
